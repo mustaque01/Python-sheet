880ffaf1a60fbe3c0ae2dc1e42e835ce4a1d7ba2
MD MUSTAK <118537720+mustaque01@users.noreply.github.com>
2024-11-18 13:36:51 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,7 @@
+def get_first_value(numbers):
+    return numbers[0]
+
+# Examples
+print(get_first_value([1, 2, 3]))       # ➞ 1
+print(get_first_value([80, 5, 100]))    # ➞ 80
+print(get_first_value([-500, 0, 50]))   # ➞ -500
