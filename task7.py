4c6f68e76787d0a8e18185a7037fb45311ee7c63
MD MUSTAK <118537720+mustaque01@users.noreply.github.com>
2024-11-19 08:59:08 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,8 @@
+def name_string(name):
+    result = name + "Happy"
+    return result
+
+# Examples
+print(name_string("Mubashir"))  # ➞ "MubashirHappy"
+print(name_string("Matt"))      # ➞ "MattHappy"
+print(name_string("python"))    # ➞ "pythonHappy"
