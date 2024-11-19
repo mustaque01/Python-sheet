e19811ea12ad090dc52928812efc50f29cecae06
MD MUSTAK <118537720+mustaque01@users.noreply.github.com>
2024-11-19 08:58:37 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,7 @@
+def animals(chickens, cows, pigs):
+    return (chickens * 2) + (cows * 4) + (pigs * 4)
+
+# Examples
+print(animals(2, 3, 5))  # ➞ 36
+print(animals(1, 2, 3))  # ➞ 22
+print(animals(5, 2, 8))  # ➞ 50
